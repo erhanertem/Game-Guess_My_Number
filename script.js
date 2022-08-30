61cd95994bc92372af5af9c431d41d6ffb06483d
Erhan Ertem <eertem@gmail.com>
2022-08-30 13:28:00 +0300

    Exercise on addEventListener()

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 // document.querySelector('#message'); //if it was an id selector
 // console.log(document.querySelector('.message')); // selects the element with class of 'message'
 // console.log(document.querySelector('.message').textContent); //selects the element's text content  which has a class of 'message'
@@ -15,3 +15,14 @@ document.querySelector('.score').textContent = 22;
 console.log(document.querySelector('.guess').value); //initially null
 document.querySelector('.guess').value = 22; //assign input a value of 22
 console.log(document.querySelector('.guess').value); // 22
+
+//Add evenhandler JS function on a selected DOM element with a class 'check'
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '🚫 No Number!';
+  }
+});
+*/
